851ea6c2aecb65dee8a5489c19dc4a66b6ded578
AnthonyM5 <anthony.mai530@gmail.com>
2021-04-06 00:11:23 -0400

    set up posts/static rendering

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+function About(){
+    return (
+        <p>My professional background is in E-commerce, and have held roles from front facing client services to back-office inventory planning/fulfillment. As an avid believer in continious learning, I have been lucky enough to have found a path to becoming a developer.</p>
+    )
+
+}
+
+export default About
